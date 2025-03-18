2891705fdc1b70df5136c9c11786dad678499137
Perry Miller <perry@modelspace.com>
2025-03-18 02:37:39 -0400

    Added comments and converted spaces to tabs

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,3 +1,18 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Copyright (c) 2025, Perry L Miller IV
+//	All rights reserved.
+//	MIT License: https://opensource.org/licenses/mit-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//	React rendering starts here.
+//
+///////////////////////////////////////////////////////////////////////////////
+
 import { App } from "./App"
 import { createRoot } from "react-dom/client"
 import { CssBaseline } from "@mui/material";
@@ -5,11 +20,18 @@ import { StrictMode } from "react"
 import { theme } from "./Theme";
 import { ThemeProvider } from "@mui/material/styles";
 
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Top of the React rendering hierarchy.
+//
+///////////////////////////////////////////////////////////////////////////////
+
 createRoot ( ( document.getElementById ( "root" ) )! ).render (
-  <StrictMode>
-    <ThemeProvider theme = { theme }>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+	<StrictMode>
+		<ThemeProvider theme = { theme }>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</StrictMode>
 );
