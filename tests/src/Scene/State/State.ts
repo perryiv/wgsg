614dbccfa3b3cf12bd1d2fa4680123768546bef6
Perry Miller <perry@modelspace.com>
2025-04-05 01:22:27 -0400

    Added tests to state class

--- tests/src/Scene/State/State.ts
+++ tests/src/Scene/State/State.ts
@@ -26,11 +26,37 @@ export function test ()
 {
 	describe ( "State Class", function ()
 	{
+		const vs = "I am the vertex shader";
+		const fs = "I am the fragment shader";
+
 		it ( "Should be able to make a state", function ()
 		{
 			const state = new State();
 			expect ( state ).to.exist;
 			expect ( state instanceof State ).to.be.true;
+		} );
+
+		it ( "Default state should have null shaders", function ()
+		{
+			const state = new State();
+			expect ( state.vertexShader ).to.be.null;
+			expect ( state.fragmentShader ).to.be.null;
+		} );
+
+		it ( "Should be able to set the shaders", function ()
+		{
+			const state = new State();
+			state.vertexShader = vs;
+			state.fragmentShader = fs;
+			expect ( state.vertexShader ).to.equal ( vs );
+			expect ( state.fragmentShader ).to.equal ( fs );
+		} );
+
+		it ( "Should be able to construct with shaders", function ()
+		{
+			const state = new State ( vs, fs );
+			expect ( state.vertexShader ).to.equal ( vs );
+			expect ( state.fragmentShader ).to.equal ( fs );
 		} );
 	} );
 };
