a29ab9da5b4d9d68b3f1b2c3bbc8b1f49e9b2e3b
Perry Miller <perry@modelspace.com>
2025-04-05 01:30:38 -0400

    Added tests to shape class

--- tests/src/Scene/Nodes/Shapes/Shape.ts
+++ tests/src/Scene/Nodes/Shapes/Shape.ts
@@ -13,7 +13,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 import { expect } from "chai";
-import { Shape } from "wgsg-lib";
+import { Shape, State } from "wgsg-lib";
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -31,6 +31,29 @@ export function test ()
 			const shape = new Shape();
 			expect ( shape ).to.exist;
 			expect ( shape instanceof Shape ).to.be.true;
+		} );
+
+		it ( "Default shape should have null state", function ()
+		{
+			const shape = new Shape();
+			expect ( shape.state ).to.be.null;
+		} );
+
+		it ( "Should be able to set the state", function ()
+		{
+			const shape = new Shape();
+			shape.state = new State();
+			expect ( shape.state ).to.exist;
+			expect ( shape.state instanceof State ).to.be.true;
+		} );
+
+		it ( "Should be able to construct with state", function ()
+		{
+			const state = new State();
+			const shape = new Shape ( state );
+			expect ( shape.state ).to.exist;
+			expect ( shape.state instanceof State ).to.be.true;
+			expect ( shape.state ).to.equal ( state );
 		} );
 	} );
 };
