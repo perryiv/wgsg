08b4c166ad31a6aac19c14666f2fef6104c3e816
Perry Miller <perry@modelspace.com>
2025-04-04 23:30:01 -0400

    Testing more tool functions

--- tests/src/WebGPU.ts
+++ tests/src/WebGPU.ts
@@ -14,7 +14,11 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 import { expect } from "chai";
-import { getData } from "wgsg-lib";
+import {
+	getData,
+	getDevice,
+	getRenderingContext,
+} from "wgsg-lib";
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -29,18 +33,35 @@ describe ( "WebGPU", function ()
 	{
 		expect ( navigator ).to.exist;
 		expect ( navigator instanceof Navigator ).to.be.true;
-
 		const { gpu } = navigator;
 		expect ( gpu ).to.exist;
 		expect ( gpu instanceof GPU ).to.be.true;
 	} );
 
-	it ( "getData", async function ()
+	it ( "Use the getData function", async function ()
 	{
 		const { adapter, device } = await getData();
 		expect ( adapter ).to.exist;
 		expect ( device ).to.exist;
 		expect ( adapter instanceof GPUAdapter ).to.be.true;
 		expect ( device instanceof GPUDevice ).to.be.true;
+	} );
+
+	it ( "Use the getDevice function", async function ()
+	{
+		const device = await getDevice();
+		expect ( device ).to.exist;
+		expect ( device instanceof GPUDevice ).to.be.true;
+	} );
+
+	it ( "Use the getRenderingContext function", async function ()
+	{
+		const canvas = document.createElement ( "canvas" );
+		expect ( canvas ).to.exist;
+		expect ( canvas instanceof HTMLCanvasElement ).to.be.true;
+		const device = await getDevice();
+		const context = getRenderingContext ( device, canvas );
+		expect ( context ).to.exist;
+		expect ( context instanceof GPUCanvasContext ).to.be.true;
 	} );
 } );
