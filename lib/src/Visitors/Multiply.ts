89c4e90eaf6b3e850ccf2d67e9d6f70f323c75d9
Perry Miller <perry@modelspace.com>
2025-05-03 21:11:31 -0400

    Cull visitor now inherits from matrix-multiplying base class

--- lib/src/Visitors/Multiply.ts
+++ lib/src/Visitors/Multiply.ts
@@ -8,23 +8,28 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//	Cull visitor class.
+//	Visitor class that modifies its matrix as it traverses the scene.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+import { mat4 } from "gl-matrix";
 import { Group, Node, Shape, Transform } from "../Scene/index";
-import { Multiply } from "./Multiply";
+import { IDENTITY_MATRIX } from "../Tools";
+import { Matrix44 } from "../Types";
+import { Visitor } from "./Visitor";
 
 
 ///////////////////////////////////////////////////////////////////////////////
 /**
- * Cull visitor class.
- * @class
+ * Matrix multiplying visitor class.
+ * @abstract
  */
 ///////////////////////////////////////////////////////////////////////////////
 
-export class Cull extends Multiply
+export abstract class Multiply extends Visitor
 {
+	#matrix: Matrix44 = [ ...IDENTITY_MATRIX ];
+
 	/**
 	 * Construct the class.
 	 * @constructor
@@ -36,21 +41,35 @@ export class Cull extends Multiply
 	}
 
 	/**
-	 * Return the class name.
-	 * @return {string} The class name.
+	 * Return the matrix.
+	 * @param {Matrix44} matrix - The matrix.
 	 */
-	public getClassName() : string
+	public get matrix()
 	{
-		return "Cull";
+		return this.#matrix;
+	}
+
+	/**
+	 * Visit the transform.
+	 */
+	public visitTransform ( transform: Transform ) : void
+	{
+		// Make a copy of the original matrix.
+		const original: Matrix44 = [ ...this.#matrix ];
+
+		// Multiply the original matrix by the given one and save it in our member.
+		mat4.multiply ( this.#matrix, original, transform.matrix );
+
+		// Now call the base class's function.
+		this.visitTransform ( transform );
+
+		// Put things back where we found them.
+		mat4.copy ( this.#matrix, original );
 	}
 
 	/**
 	 * Visit these node types.
 	 */
-	public visitTransform ( tr: Transform ) : void
-	{
-		super.visitTransform ( tr );
-	}
 	public visitGroup ( group: Group ) : void
 	{
 		super.visitGroup ( group );
