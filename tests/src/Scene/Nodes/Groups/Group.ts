561856513e68e25a2b79df22b6bb0a0933da9d79
Perry Miller <perry@modelspace.com>
2025-07-01 00:21:38 -0400

    Added tests for new traverse function and bug fixes

--- tests/src/Scene/Nodes/Groups/Group.ts
+++ tests/src/Scene/Nodes/Groups/Group.ts
@@ -13,7 +13,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 import { expect } from "chai";
-import { Group } from "wgsg-lib";
+import { Group, Node } from "wgsg-lib";
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -131,6 +131,161 @@ export function test ()
 			const message = "Given group is already a parent of this node";
 			expect ( () => { root.addChild ( child ); } ).to.throw ( message );
 			expect ( root.size ).to.equal ( 1 );
+		} );
+
+		it ( "Should be able to clear all child nodes", function ()
+		{
+			const root = new Group();
+			const child0 = new Group();
+			const child1 = new Group();
+
+			root.addChild ( child0 );
+			root.addChild ( child1 );
+			expect ( root.size ).to.equal ( 2 );
+
+			root.clear();
+			expect ( root.size ).to.equal ( 0 );
+		} );
+
+		it ( "Should be able to remove all child nodes", function ()
+		{
+			const root = new Group();
+			const child0 = new Group();
+			const child1 = new Group();
+
+			root.addChild ( child0 );
+			root.addChild ( child1 );
+			expect ( root.size ).to.equal ( 2 );
+
+			root.removeChild ( 0 );
+			expect ( root.size ).to.equal ( 1 );
+
+			root.removeChild ( 0 );
+			expect ( root.size ).to.equal ( 0 );
+		} );
+
+		it ( "Should be able to get child nodes", function ()
+		{
+			const root = new Group();
+			const child0 = new Group();
+			const child1 = new Group();
+
+			root.addChild ( child0 );
+			root.addChild ( child1 );
+			expect ( root.size ).to.equal ( 2 );
+
+			expect ( root.getChild ( 0 ) ).to.equal ( child0 );
+			expect ( root.getChild ( 1 ) ).to.equal ( child1 );
+			expect ( root.getChild ( 2 ) ).to.be.null;
+		} );
+
+		it ( "Can traverse the group", function ()
+		{
+			const root = new Group();
+			const child0 = new Group();
+			const child1 = new Group();
+			const child2 = new Group();
+
+			root.addChild ( child0 );
+			root.addChild ( child1 );
+			root.addChild ( child2 );
+
+			child0.addChild ( new Group() );
+			child0.addChild ( new Group() );
+			child0.addChild ( new Group() );
+			child0.addChild ( new Group() );
+			child1.addChild ( new Group() );
+			child1.addChild ( new Group() );
+			child1.addChild ( new Group() );
+			child1.addChild ( new Group() );
+			child2.addChild ( new Group() );
+			child2.addChild ( new Group() );
+			child2.addChild ( new Group() );
+			child2.addChild ( new Group() );
+
+			let count = 0;
+			root.traverse ( () => { ++count; } );
+			expect ( count ).to.equal ( 16 );
+		} );
+
+		it ( "Should be initially dirty", function ()
+		{
+			const root = new Group();
+			const child0 = new Group();
+			const child1 = new Group();
+
+			root.addChild ( child0 );
+			root.addChild ( child1 );
+
+			expect ( root.dirty ).to.be.true;
+			expect ( child0.dirty ).to.be.true;
+			expect ( child1.dirty ).to.be.true;
+		} );
+
+		it ( "Can change the dirty state of a group", function ()
+		{
+			const group = new Group();
+			expect ( group.dirty ).to.be.true;
+
+			group.dirty = false;
+			expect ( group.dirty ).to.be.false;
+		} );
+
+		it ( "Can change the dirty state of a child node", function()
+		{
+			const root = new Group();
+			const child0 = new Group();
+			const child1 = new Group();
+
+			root.addChild ( child0 );
+			root.addChild ( child1 );
+
+			const child00 = new Group();
+			const child01 = new Group();
+
+			child0.addChild ( child00 );
+			child0.addChild ( child01 );
+
+			const child10 = new Group();
+			const child11 = new Group();
+
+			child1.addChild ( child10 );
+			child1.addChild ( child11 );
+
+			expect ( root.dirty ).to.be.true;
+			expect ( child0.dirty ).to.be.true;
+			expect ( child1.dirty ).to.be.true;
+			expect ( child00.dirty ).to.be.true;
+			expect ( child01.dirty ).to.be.true;
+			expect ( child10.dirty ).to.be.true;
+			expect ( child11.dirty ).to.be.true;
+
+			let count = 0;
+			root.traverse ( ( child: Node ) =>
+			{
+				expect ( child.dirty ).to.be.true;
+				child.dirty = false;
+				++count;
+			} );
+			expect ( count ).to.equal ( 7 );
+
+			expect ( root.dirty ).to.be.false;
+			expect ( child0.dirty ).to.be.false;
+			expect ( child1.dirty ).to.be.false;
+			expect ( child00.dirty ).to.be.false;
+			expect ( child01.dirty ).to.be.false;
+			expect ( child10.dirty ).to.be.false;
+			expect ( child11.dirty ).to.be.false;
+
+			child00.dirty = true;
+
+			expect ( root.dirty ).to.be.true;
+			expect ( child0.dirty ).to.be.true;
+			expect ( child1.dirty ).to.be.false;
+			expect ( child00.dirty ).to.be.true;
+			expect ( child01.dirty ).to.be.false;
+			expect ( child10.dirty ).to.be.false;
+			expect ( child11.dirty ).to.be.false;
 		} );
 	} );
 };
