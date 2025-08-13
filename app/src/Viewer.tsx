b2bdd80a8e8e072240614993267a49a687b95c2a
Perry Miller <perry@modelspace.com>
2025-08-13 00:30:22 -0400

    Default state name now works

--- app/src/Viewer.tsx
+++ app/src/Viewer.tsx
@@ -85,7 +85,6 @@ const makeQuad = ( origin: IVector3, size: IVector2, color: IVector4 ) =>
 	shader.color = color;
 
 	const state = new State();
-	state.name = `TriangleSolidColor with color [${color.join ( ", " )}]`;
 	state.shader = shader;
 
 	geom.state = state;
