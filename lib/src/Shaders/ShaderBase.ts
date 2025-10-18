4ce57cb865521c76f7a3b66508dc96b7490b00e0
Perry Miller <perry@modelspace.com>
2025-10-18 00:59:30 -0400

    Steps towards handling when the device changes

--- lib/src/Shaders/ShaderBase.ts
+++ lib/src/Shaders/ShaderBase.ts
@@ -42,7 +42,12 @@ export abstract class ShaderBase extends Base
 {
 	#code: string;
 	#module: ( GPUShaderModule | null ) = null;
+<<<<<<< Updated upstream
 	#pipeline: ( GPURenderPipeline | null ) = null;
+=======
+	#data: IPipelineTopologyPair = { pipeline: null, topology: null };
+	#device = 0;
+>>>>>>> Stashed changes
 
 	/**
 	 * Construct the class.
@@ -70,7 +75,12 @@ export abstract class ShaderBase extends Base
 	public destroy() : void
 	{
 		this.#module = null;
+<<<<<<< Updated upstream
 		this.#pipeline = null;
+=======
+		this.#data = { pipeline: null, topology: null };
+		this.#device = 0;
+>>>>>>> Stashed changes
 	}
 
 	/**
@@ -106,7 +116,16 @@ export abstract class ShaderBase extends Base
 	 */
 	public get module() : GPUShaderModule
 	{
-		// The first time we have to make it.
+		// Did the device change?
+		const device = Device.instance.id;
+		if ( this.#device !== device )
+		{
+			this.#device = device;
+			this.#module = null;
+			this.#data = { pipeline: null, topology: null };
+		}
+
+		// Make it if we have to.
 		if ( !this.#module )
 		{
 			this.#module = Device.instance.device.createShaderModule ( {
