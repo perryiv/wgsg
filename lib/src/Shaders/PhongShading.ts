2fa565a2d92266f1a37f3747391c22f2a6a27e0a
Perry Miller <perry@modelspace.com>
2026-04-23 01:04:36 -0400

    Added shader for Phong shading algorithm

---
+++ lib/src/Shaders/PhongShading.ts
@@ -0,0 +1,372 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Copyright (c) 2025, Perry L Miller IV
+//	All rights reserved.
+//	MIT License: https://opensource.org/licenses/mit-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Shader class that renders with Phong shading.
+//	https://webgpufundamentals.org/webgpu/lessons/webgpu-lighting-directional.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import { Device } from "../Tools";
+import { WithMatrices as BaseClass } from "./WithMatrices";
+import { vec4 } from "gl-matrix";
+import type { IMatrix44, IVector4 } from "../Types";
+
+// @ts-expect-error TypeScript does not recognize WGSL files.
+import code from "./PhongShading.wgsl?raw";
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Types used below.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+interface IPhongShadingShaderInput
+{
+	topology?: GPUPrimitiveTopology;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Class that contains the shader code.
+ * @class
+ */
+///////////////////////////////////////////////////////////////////////////////
+
+export class PhongShading extends BaseClass
+{
+	static #instance: ( PhongShading | null ) = null;
+	#color: IVector4 = [ 0.5, 0.5, 0.5, 1.0 ];
+	#lightDir: IVector4 = [ 0.0, 0.0, -1.0, 0.0 ];
+	#uniforms: ( GPUBuffer | null ) = null;
+	#bindGroup: ( GPUBindGroup | null ) = null;
+
+	/**
+	 * Construct the class.
+	 * @class
+	 * @param {IPhongShadingShaderInput} [input] - The input.
+	 */
+	protected constructor ( input?: Readonly<IPhongShadingShaderInput> )
+	{
+		const topology = input?.topology;
+
+		super ( {
+			code: ( code as string ),
+			topology: ( topology ?? "triangle-list" )
+		} );
+	}
+
+	/**
+	 * Destroy this instance.
+	 */
+	public override destroy() : void
+	{
+		this.reset();
+		super.destroy();
+	}
+
+	/**
+	 * Reset the shader.
+	 */
+	public override reset() : void
+	{
+		if ( this.#uniforms )
+		{
+			this.#uniforms.destroy();
+			this.#uniforms = null;
+		}
+		this.#bindGroup = null;
+		super.reset();
+	}
+
+	/**
+	 * Get the singleton instance.
+	 * @returns {PhongShading} The singleton instance.
+	 */
+	public static get instance () : PhongShading
+	{
+		if ( !PhongShading.#instance )
+		{
+			PhongShading.#instance = new PhongShading();
+		}
+		return PhongShading.#instance;
+	}
+
+	/**
+	 * Destroy the singleton instance.
+	 */
+	public static destroy() : void
+	{
+		if ( PhongShading.#instance )
+		{
+			PhongShading.#instance.destroy();
+			PhongShading.#instance = null;
+		}
+	}
+
+	/**
+	 * Return the class name.
+	 * @returns {string} The class name.
+	 */
+	public override getClassName() : string
+	{
+		return "Shaders.PhongShading";
+	}
+
+	/**
+	 * Get the name.
+	 * @returns {string} The name of the shader.
+	 */
+	public get name() : string
+	{
+		const color = this.color.join ( ", " );
+		const lightDir = this.lightDir.join ( ", " );
+		return `${this.type} with color [${color}] and light direction [${lightDir}]`;
+	}
+
+	/**
+	 * Get the view matrix.
+	 * @returns {IMatrix44} The view matrix.
+	 */
+	public override get viewMatrix () : Readonly<IMatrix44>
+	{
+		return super.viewMatrix;
+	}
+
+	/**
+	 * Set the view matrix. Overload if needed.
+	 * @param {IMatrix44} matrix - The view matrix.
+	 */
+	public override set viewMatrix ( matrix: Readonly<IMatrix44> )
+	{
+		super.viewMatrix = matrix;
+		this.#uniforms = null;
+		this.#bindGroup = null;
+	}
+
+	/**
+	 * Return the color.
+	 * @returns {IVector4} The color.
+	 */
+	public get color () : IVector4
+	{
+		return [ ...this.#color ];
+	}
+
+	/**
+	 * Set the color.
+	 * @param {IVector4} color - The color.
+	 */
+	public set color ( color: Readonly<IVector4> )
+	{
+		vec4.copy ( this.#color, color );
+		this.#uniforms = null;
+		this.#bindGroup = null;
+	}
+
+	/**
+	 * Return the light direction.
+	 * @returns {IVector4} The light direction.
+	 */
+	public get lightDir () : IVector4
+	{
+		return [ ...this.#lightDir ];
+	}
+
+	/**
+	 * Set the light direction.
+	 * @param {IVector4} lightDir - The light direction.
+	 */
+	public set lightDir ( lightDir: Readonly<IVector4> )
+	{
+		vec4.copy ( this.#lightDir, lightDir );
+		this.#uniforms = null;
+		this.#bindGroup = null;
+	}
+
+	/**
+	 * Return the uniform buffer.
+	 * @returns {GPUBuffer | null} The uniform buffer.
+	 */
+	protected get uniforms () : GPUBuffer
+	{
+		// Shortcut
+		let buffer = this.#uniforms;
+
+		// Make it if we have to.
+		if ( !buffer )
+		{
+			// Shortcut
+			const device = Device.instance.device;
+
+			// Create the buffer.
+			buffer = device.createBuffer ( {
+				label: `Uniform buffer for shader ${this.type} ${this.id}`,
+				size: ( 16 + 16 + 4 + 4 ) * 4, // Two 4x4 matrices + 4D color + 4D light, 4 bytes each.
+				usage: ( GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST )
+			} );
+
+			// Write the values to a typed array.
+			const pm = new Float32Array ( this.projMatrix );
+			const vm = new Float32Array ( this.viewMatrix );
+			const color = new Float32Array ( this.#color );
+			const lightDir = new Float32Array ( this.#lightDir );
+
+			// Write the typed array to the buffer.
+			let offset = 0;
+			device.queue.writeBuffer ( buffer, offset, pm       ); offset += pm.byteLength;
+			device.queue.writeBuffer ( buffer, offset, vm       ); offset += vm.byteLength;
+			device.queue.writeBuffer ( buffer, offset, color    ); offset += color.byteLength;
+			device.queue.writeBuffer ( buffer, offset, lightDir );
+
+			// Set this for next time.
+			this.#uniforms = buffer;
+		}
+
+		// Return what we have.
+		return buffer;
+	}
+
+	/**
+	 * Make the render pipeline.
+	 * @param {GPUPrimitiveTopology} topology - The primitive topology.
+	 * @returns {GPURenderPipeline} The render pipeline.
+	 */
+	protected makePipeline ( topology: Readonly<GPUPrimitiveTopology> ) : GPURenderPipeline
+	{
+		// Shortcuts
+		const { device, preferredFormat } = Device.instance;
+
+		// Define the array stride.
+		// https://www.w3.org/TR/webgpu/#enumdef-gpuvertexstepmode
+		const arrayStride = 12; // 3 floats * 4 bytes each.
+
+		// Make the pipeline.
+		const pipeline = device.createRenderPipeline ( {
+			label: `Render pipeline for shader ${this.type}`,
+			layout: "auto",
+			vertex: {
+				module: this.module,
+				entryPoint: "vs",
+				buffers: [
+				{
+					attributes: [
+					{
+						// Position
+						shaderLocation: 0,
+						offset: 0,
+						format: "float32x3",
+					} ],
+					arrayStride,
+					stepMode: "vertex",
+				}, {
+					attributes: [
+					{
+						// Normal vector
+						shaderLocation: 1,
+						offset: 0,
+						format: "float32x3",
+					} ],
+					arrayStride,
+					stepMode: "vertex",
+				} ]
+			},
+			fragment: {
+				module: this.module,
+				entryPoint: "fs",
+				targets: [ {
+					format: preferredFormat,
+					blend: {
+						color: {
+							srcFactor: "one",
+							dstFactor: "one-minus-src-alpha"
+						},
+						alpha: {
+							srcFactor: "one",
+							dstFactor: "one-minus-src-alpha"
+						},
+					},
+				} ]
+			},
+			primitive: {
+				topology
+			},
+			depthStencil: {
+				depthWriteEnabled: true,
+				depthCompare: "less",
+				format: "depth24plus",
+			},
+		} );
+
+		// Do not return an invalid pipeline.
+		if ( !pipeline )
+		{
+			throw new Error ( "Failed to create pipeline" );
+		}
+
+		// Return the new pipeline.
+		return pipeline;
+	}
+
+	/**
+	 * Get the bind group.
+	 * @param {GPUPrimitiveTopology} topology - The primitive topology.
+	 * @returns {GPUBindGroup} The bind group.
+	 */
+	protected getBindGroup ( topology: Readonly<GPUPrimitiveTopology> ) : GPUBindGroup
+	{
+		// Shortcut
+		let bindGroup = this.#bindGroup;
+
+		// Make it if we have to.
+		if ( !bindGroup )
+		{
+			// Shortcuts.
+			const pipeline = this.getPipeline ( topology );
+			const device = Device.instance.device;
+
+			// Make the layout and give it a name.
+			const index = 0;
+			const layout = pipeline.getBindGroupLayout ( index );
+			layout.label = `Bind group layout ${index} for shader ${this.type}`;
+
+			// Make the bind group.
+			bindGroup = device.createBindGroup ( {
+				label: `Bind group for shader ${this.type}`,
+				layout,
+				entries: [
+				{
+					binding: 0,
+					resource: { buffer: this.uniforms }
+				} ],
+			} );
+
+			// Set for next time.
+			this.#bindGroup = bindGroup;
+		}
+
+		// Return the bind group.
+		return bindGroup;
+	}
+
+	/**
+	 * Configure the render pass.
+	 * @param {GPURenderPassEncoder} pass - The render pass encoder.
+	 * @param {GPUPrimitiveTopology} topology - The primitive topology.
+	 */
+	public configureRenderPass ( pass: Readonly<GPURenderPassEncoder>, topology: Readonly<GPUPrimitiveTopology> ) : void
+	{
+		// Note: The render-pass' pipeline should already be set.
+
+		// Set the color buffer.
+		pass.setBindGroup ( 0, this.getBindGroup ( topology ) );
+	}
+}
---
+++ lib/src/Shaders/PhongShading.wgsl
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Copyright (c) 2025, Perry L Miller IV
+//	All rights reserved.
+//	MIT License: https://opensource.org/licenses/mit-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//	Shader code that renders with Phong shading.
+//	https://gist.github.com/ccincotti3/f5bbfca9acd27c0efb9a2d22509b5aca
+//
+///////////////////////////////////////////////////////////////////////////////
+
+struct Uniforms
+{
+	projMatrix: mat4x4f,
+	viewMatrix: mat4x4f,
+	color: vec4f,
+	lightDir: vec3f,
+};
+
+@group ( 0 ) @binding ( 0 ) var<uniform> uniforms : Uniforms;
+
+struct VertexOut
+{
+	@builtin ( position ) position : vec4f,
+	@location ( 0 ) normal : vec3f,
+};
+
+@vertex fn vs ( @location ( 0 ) position: vec4f, @location ( 1 ) normal: vec3f ) -> VertexOut
+{
+	// The answer.
+	var answer : VertexOut;
+
+	// Transform the position to view space.
+	answer.position = uniforms.projMatrix * uniforms.viewMatrix * position;
+
+	// Transform the normal to view space. We ignore the translation.
+	answer.normal = normalize ( ( uniforms.viewMatrix * vec4f ( normal, 0.0 ) ).xyz );
+
+	// Return the answer.
+	return answer;
+}
+
+@fragment fn fs ( fragData: VertexOut ) -> @location ( 0 ) vec4f
+{
+	// Make sure the normal vector is unit length.
+	let normal = normalize ( fragData.normal );
+
+	// Calculate the diffuse lighting factor.
+	let diffuse = max ( dot ( normal, -uniforms.lightDir ), 0.0 );
+
+	// Calculate the color, assuming that the canvas is configured
+	// for pre-multiplied alpha.
+	return vec4 ( uniforms.color.rgb * diffuse * uniforms.color.a, uniforms.color.a );
+}
