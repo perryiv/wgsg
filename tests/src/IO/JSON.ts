15b9da7848f53117b859320d528233cb1c9bf17e
Perry Miller <perry@modelspace.com>
2026-05-22 20:19:02 -0400

    Fixed test

--- tests/src/IO/JSON.ts
+++ tests/src/IO/JSON.ts
@@ -113,12 +113,44 @@ export function test ()
 	{
 		it ( "Should be able to write a meaningful JSON file", function ()
 		{
-			const scene = buildSceneSphere ( new Sphere ( [ 0, 0, 0 ], 1 ), true );
-			let json: ( string | object ) = JSON.stringify ( scene );
+			const expected = {
+				id: 1,
+				type: "Scene.Nodes.Groups.Group",
+				state: null,
+				flags: 31,
+				bounds: {
+					center: [ 0, 0, 0	],
+					radius: 1
+				},
+				children: [ {
+					id: 1,
+					type: "Scene.Nodes.Shapes.Sphere",
+					state: null,
+					flags: 27,
+					bounds: {
+						center: [ 0, 0, 0 ],
+						radius: 1
+					},
+					box: {
+						min: [ -1, -1, -1 ],
+						max: [ 1, 1, 1 ]
+					},
+					center: [ 0, 0, 0 ],
+					radius: 1,
+					numSubdivisions: 2
+				} ]
+			};
+
+			const scene = new Group();
+			scene.addChild ( new SphereNode() );
+
+			let json: ( string | object ) = JSON.stringify ( scene, null, 2 );
 			expect ( json ).to.be.a ( "string" );
+
+			// console.log ( "json:", json );
+
 			json = JSON.parse ( json ) as object;
-			console.log ( "json:", json );
-			expect ( json ).to.be.deep.equal ( {} );
+			expect ( json ).to.be.deep.equal ( expected );
 		} );
 	} );
 };
