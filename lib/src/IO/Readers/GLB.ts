852425ade95a47ddb05a9e7a5da27d55d24f4a09
Perry Miller <perry@modelspace.com>
2026-09-10 01:13:38 -0400

    Reading JSON

--- lib/src/IO/Readers/GLB.ts
+++ lib/src/IO/Readers/GLB.ts
@@ -54,14 +54,17 @@ interface ChunkHeaderResult
 
 
 ///////////////////////////////////////////////////////////////////////////////
-//
-//	Read the header of the GLB file.
-//
+/**
+ * Read the header of the GLB file.
+ * @param {File} file The GLB file to read.
+ * @returns {Promise<FileHeaderResult>} A promise that resolves with the file header and the offset after the header.
+ */
 ///////////////////////////////////////////////////////////////////////////////
 
-const readFileHeader = async ( file: File ) : Promise < FileHeaderResult >
+const readFileHeader = async ( file: File ) : Promise < FileHeaderResult > =>
 {
-	const result = await readFile ( file, "ArrayBuffer", 0, GLB_FILE_HEADER_SIZE );
+	const end = GLB_FILE_HEADER_SIZE;
+	const result = await readFile ( file, "ArrayBuffer", 0, end );
 
 	// Make the view for the header data.
 	const buffer = ( result as ArrayBuffer );
@@ -72,19 +75,6 @@ const readFileHeader = async ( file: File ) : Promise < FileHeaderResult >
 	const version = view.getUint32 ( 4, true );
 	const length  = view.getUint32 ( 8, true );
 
-	// Return the answer.
-	return { header: { magic, version, length }, offset: GLB_FILE_HEADER_SIZE };
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//	Check the header of the GLB file.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-const checkFileHeader = async ( file: File, { magic, version, length }: FileHeader )
-{
 	// Make sure the magic number is correct.
 	if ( magic !== 0x46546C67 ) // ASCII for "glTF".
 	{
@@ -102,6 +92,59 @@ const checkFileHeader = async ( file: File, { magic, version, length }: FileHead
 	{
 		throw new Error ( `Invalid GLB file length: expected ${length}, got ${file.size}` );
 	}
+
+	// Return the answer.
+	return { header: { magic, version, length }, offset: end };
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Read the header of the next chunk of data.
+ * @param {File} file The file to read.
+ * @param {number} offset The offset in the file where the chunk header starts.
+ * @returns {Promise<ChunkHeaderResult>} A promise that resolves with the chunk header and the offset after the header.
+ */
+///////////////////////////////////////////////////////////////////////////////
+
+const readChunkHeader = async ( file: File, offset: number ) : Promise < ChunkHeaderResult > =>
+{
+	const end = offset + GLB_CHUNK_HEADER_SIZE;
+	const result = await readFile ( file, "ArrayBuffer", offset, end );
+
+	// Make the view for the chunk header data.
+	const buffer = ( result as ArrayBuffer );
+	const view = new DataView ( buffer );
+
+	// Get the chunk header.
+	const length = view.getUint32 ( 0, true );
+	const type   = view.getUint32 ( 4, true );
+
+	// Return the answer.
+	return { header: { length, type }, offset: end };
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Read the header of the next chunk of data.
+ * @param {File} file The file to read.
+ * @param {ChunkHeaderResult} result The result from reading the chunk header.
+ * @returns {Promise<string>} A promise that resolves with the JSON string from the chunk.
+ */
+///////////////////////////////////////////////////////////////////////////////
+
+const readJSON = async ( file: File, result: ChunkHeaderResult ) : Promise < string > =>
+{
+	const end = result.offset + result.header.length;
+	const text = await readFile ( file, "Text", result.offset, end );
+
+	if ( "string" !== ( typeof text ) )
+	{
+		throw new Error ( `Failed to read JSON chunk as text` );
+	}
+
+	return ( text as string );
 }
 
 
@@ -144,21 +187,6 @@ class GLB extends BaseClass
 	}
 
 	/**
-	 * Create an error object from a message and a FileReader progress event.
-	 * @param {string} message The error message.
-	 * @param {ProgressEvent<FileReader>} event The progress event from the FileReader.
-	 * @returns {Error} The constructed error object.
-	 */
-	protected static makeError ( message: string, event: ProgressEvent < FileReader > ) : Error
-	{
-			if ( event.target?.error )
-			{
-				message = `${message}, ${event.target.error.message}`;
-			}
-			return ( new Error ( message ) );
-	}
-
-	/**
 	 * Read the file and return a promise that resolves to the scene node.
 	 * @param {File} file The file to read.
 	 * @returns {Promise<SceneNode>} A promise that resolves to the scene node.
@@ -169,76 +197,21 @@ class GLB extends BaseClass
 		this.#file = file.name;
 
 		// Read the header information.
-		const { header, offset } = await readFileHeader ( file );
+		const result1 = await readFileHeader ( file );
 
-		// Check the header.
-		checkFileHeader ( file, header );
+		// Read the JSON header.
+		const result2 = await readChunkHeader ( file, result1.offset );
 
 		// Read the JSON data.
-		offset = await this.readJSON ( file, offset );
+		const result3 = await readJSON ( file, result2 );
 
-		// return new Promise ( ( resolve, reject ) =>
-		// {
-		// 	this.readFileHeader ( file );
-			// .then ( ( offset: number ) =>
-			// {
-			// 	return this.readChunkHeader ( file, offset );
-			// } )
-			// .then ( ( data: { length: number, type: number, offset: number } ) =>
-			// {
-			// 	const { length, type, offset } = data;
-			// 	return this.readChunkData ( file, length, type, offset );
-			// } )
-			// .then ( () =>
-			// {
-			// 	resolve ( new Group() );
-			// } )
-			// .catch ( ( error: Error ) =>
-			// {
-			// 	reject ( error );
-			// } );
-		// } );
+		// Convert it to an object.
+		const result4 = JSON.parse ( result3 ) as Record < string, unknown >;
+
+		// We only handle triangle meshes.
+		console.debug ( result4 );
 
 		return new Group();
-	}
-
-	/**
-	 * Read the header of the next chunk of data.
-	 * @param {File} file The file to read.
-	 * @param {number} offset The offset in the file where the chunk header starts.
-	 * @returns {Promise<number>} A promise that resolves with the size of the chunk header when it has been read.
-	 */
-	protected readChunkHeader ( file: File, offset: number ) : Promise < { length: number, type: number, offset: number } >
-	{
-		return new Promise ( ( resolve, reject ) =>
-		{
-			// Make the reader.
-			const reader = new FileReader();
-
-			// This is called if there is an error.
-			reader.onerror = ( event: ProgressEvent < FileReader > ) =>
-			{
-				reject ( GLB.makeError ( `Error reading data chunk header at offset ${offset} for file '${file.name}'`, event ) );
-			}
-
-			// This is called when the data is ready.
-			reader.onload = () =>
-			{
-				// Make the view for the chunk header data.
-				const buffer = ( reader.result as ArrayBuffer );
-				const view = new DataView ( buffer );
-
-				// Get the chunk header.
-				const length = view.getUint32 ( 0, true );
-				const type   = view.getUint32 ( 4, true );
-
-				// We succeeded.
-				resolve ( { length, type, offset } );
-			}
-
-			// Read the chunk header.
-			reader.readAsArrayBuffer ( file.slice ( offset, offset + GLB_CHUNK_HEADER_SIZE ) );
-		} );
 	}
 }
 
